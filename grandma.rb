bdbe0f3ac3e16ae52c954d495c462d4b17a3293e
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-09 19:53:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase
-    "HUH?! SPEAK UP, SONNY!"
-  else
+  elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
+  else
+   "HUH?! SPEAK UP, SONNY!"
   end
 end
